626bb207a964afdf696737d0c08d4690ccf3d7c2
Gousia-MSFT <45553518+Gousia-MSFT@users.noreply.github.com>
2019-02-07 15:14:14 +0530

    added MIT License to all source files and added external styles

--- Cross Vertical/CompanyCommunicator/Global.asax.cs
+++ Cross Vertical/CompanyCommunicator/Global.asax.cs
@@ -1,4 +1,26 @@
-﻿using Autofac;
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+using Autofac;
 using CrossVertical.Announcement.Helpers;
 using CrossVertical.Announcement.Repository;
 using Microsoft.Bot.Builder.Azure;
@@ -40,7 +62,7 @@ namespace CrossVertical.Announcement
 
                 // To use CosmosDb or InMemory storage instead of the default table storage, uncomment the corresponding line below
                 // var store = new DocumentDbBotDataStore("cosmos db uri", "cosmos db key"); // requires Microsoft.BotBuilder.Azure Nuget package 
-                // var store = new InMemoryDataStore(); // volatile in-memory store
+                 //var store = new InMemoryDataStore(); // volatile in-memory store
 
                 builder.Register(c => store)
                 .Keyed<IBotDataStore<BotData>>(AzureModule.Key_DataStore)
