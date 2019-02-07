4acd649581c4b2be0e65f8ff68f654127881d33c
Wajeed-msft <31851992+Wajeed-msft@users.noreply.github.com>
2019-02-07 18:07:43 +0530

    V washai/announcement app latest (#45)
    * Pushing code to latest branch.
    
    * handled no announcements
    
    * history tab ui changes
    
    * Added scheduler.
    
    * history tab changes-latest
    
    * Changes to history tab & Added Details tab
    
    * Added Task Module in History Tab. Updated scheduler logic to go to sleep when no job is scheduled.
    
    * Handled edit from Tab.
    
    * Added modify schedule flow.
    
    * Fixed task module height.
    
    * History tab UI Changes-Latest
    
    * Handled schedule for old records.
    
    * Channel members count in History Tab
    
    * Updated message for update schedule.
    
    * Reduced the height of Update Schedule Task Module.
    
    * Added controller and view for analytics and audiance details.
    
    * Added partial views for Analysis Tab view.
    
    * Added analytics UI
    
    * Updated UI text and handled edit condition for sent announcements.
    
    * Analytics page -UI changes
    
    * Filled models with actula data.
    
    * tab changes-analytics
    
    * Fixed issue with deep link. Added Analysis and Audiance view.
    
    * UI alignment
    
    * Fixed use as template issue for sent card.
    
    * Fixed issue with welcome message in personal scope.
    
    * ui changes analaytics
    
    * analytics tabs enabled based on count
    
    * analytics-channel count
    
    * Fixed UI issues. Fixed trigger of multiple send for large number of audiance.
    
    * Optimized image fetching for profile photo.
    
    * Changed app name to Contoso Broadcast
    
    * Fixed issue with Analytics tab.
    
    * Removed image element if user haven't specified banner image.
    
    * Removed channel message when welcome message is failed.
    
    * display user-channel like in analytics UI
    
    * Added Login and Application grant.
    
    * Change name to Company Communicator.
    
    * Added role base support for posts.
    
    * Added checks for action so that only admin or moderators are allowd to create announcement.
    
    * Added connection name in web.config file.
    
    * Added Compose Extension
    
    * Handled No announcements based on role
    
    * Added Welcome Message for User(Role) in 1:1
    
    * Added messaging extension.
    
    * Merged welcome screen changes for 1:1 conversation.
    
    * Completed messaing extenstion and 1:1 flow.
    
    * Updated list for recent. Welcome screen changes.
    
    * Fixed issue with 1:1 show recents
    
    * Code cleanup- removed commented code.
    
    * Added comment and cleanup code.
    
    * Renamed announcement app to Company Communicator. Cleaned up code.
    
    * Changed InMemoryStorage to Azure Table Storage.
    
    * Added check for re-using old groups with updated employees.
    
    * Cleaned up code.
    
    * Corrected type of image being fetched.
    
    * added MIT License to all source files and added external styles
    
    * added styles in csproj
    
    * Added Open source license (MIT license) at the top of all source files.
    Moved CSS to separate files

---
+++ Cross Vertical/CompanyCommunicator/Helpers/Common.cs
@@ -0,0 +1,73 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license.
+//
+// MIT License
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. 
+using CrossVertical.Announcement.Models;
+using CrossVertical.Announcement.Repository;
+using Microsoft.Bot.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CrossVertical.Announcement.Helpers
+{
+    public class Common
+    {
+        public static Role GetUserRole(string emailId, Tenant tenatInfo)
+        {
+            var role = Role.User;
+            if (tenatInfo.Moderators.Contains(emailId))
+                role = Role.Moderator;
+            if (tenatInfo.Admin == emailId)
+                role = Role.Admin;
+            return role;
+        }
+
+        public static async Task<List<Campaign>> GetMyAnnouncements(string emailId, string tenantId)
+        {
+            var tenant = await Cache.Tenants.GetItemAsync(tenantId);
+            Role role = GetUserRole(emailId, tenant);
+
+            var myTenantAnnouncements = new List<Campaign>();
+            foreach (var announcementId in tenant.Announcements)
+            {
+                var announcement = await Cache.Announcements.GetItemAsync(announcementId);
+                if (announcement != null)
+                {
+                    if (role == Role.Moderator || role == Role.Admin)
+                    {
+                        myTenantAnnouncements.Add(announcement);
+                    }
+                    else if (announcement.Recipients.Channels.Any(c => c.Members.Contains(emailId))
+                          || announcement.Recipients.Groups.Any(g => g.Users.Any(u => u.Id == emailId)))
+                    {
+                        // Validate if user is part of this announcement.
+                        myTenantAnnouncements.Add(announcement);
+                    }
+                }
+            }
+            return myTenantAnnouncements;
+        }
+
+    }
+}
