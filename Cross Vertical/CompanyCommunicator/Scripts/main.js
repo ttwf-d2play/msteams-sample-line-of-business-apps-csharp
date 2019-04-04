4cc7607fc9bf33ca3e8791e9f6cce4f01d3c56d5
Gousia-MSFT <45553518+Gousia-MSFT@users.noreply.github.com>
2019-04-04 18:10:46 +0530

    Fixed issues with announcemnet mobile design

--- Cross Vertical/CompanyCommunicator/Scripts/main.js
+++ Cross Vertical/CompanyCommunicator/Scripts/main.js
@@ -15,37 +15,41 @@
 //   }
 
 $(document).ready(function () {
-    $(".row1").click(function () {
-        var array = this.className.split(" ");
-        var id = array[array.length - 1];
-        alert(id);
-        $("#" + id).toggle();
+    /* let previousId;
+    $("#row1").click(function () {
+      // $(".title-down-row.row1").toggle();
     });
-    //$("#row2").click(function () {
-    //    $(".title-down-row.row2").toggle();
-    //});
-    //$("#row3").click(function () {
-    //    $(".title-down-row.row3").toggle();
-    //});
-    //$("#row4").click(function () {
-    //    $(".title-down-row.row4").toggle();
-    //});
-    //$("#row5").click(function () {
-    //    $(".title-down-row.row5").toggle();
-    //});
-    //$("#row6").click(function () {
-    //    $(".title-down-row.row6").toggle();
-    //});
-    //$("#row7").click(function () {
-    //    $(".title-down-row.row7").toggle();
-    //});
-    //$("#row8").click(function () {
-    //    $(".title-down-row.row8").toggle();
-    //});
-    //$("#row9").click(function () {
-    //    $(".title-down-row.row9").toggle();
-    //});
-    //$("#row10").click(function () {
-    //    $(".title-down-row.row10").toggle();
-    //});
+    $("#row2").click(function () {
+      if(previousId){
+        $(previousId).toggle();
+        previousId =$(".title-down-row.row1");
+      }
+      $(".title-down-row.row2").toggle();
+    });
+    $("#row3").click(function () {
+      $(".title-down-row.row3").toggle();
+      // $(".title-down-row.row1").toggle();
+    });
+    $("#row4").click(function () {
+      $(".title-down-row.row4").toggle();
+    });
+    $("#row5").click(function () {
+      $(".title-down-row.row5").toggle();
+    });
+    $("#row6").click(function () {
+      $(".title-down-row.row6").toggle();
+    });
+    $("#row7").click(function () {
+      $(".title-down-row.row7").toggle();
+    });
+    $("#row8").click(function () {
+      $(".title-down-row.row8").toggle();
+    });
+    $("#row9").click(function () {
+      $(".title-down-row.row9").toggle();
+    });
+    $("#row10").click(function () {
+      $(".title-down-row.row10").toggle();
+    }); */
 });
+
